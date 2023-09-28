43ea6973c34a1ab38273348421ea94085633c678
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-09-28 17:07:51 +0530

    did some changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const nFloors = +document.querySelector('.noOfFloors').value
-const nLifts = +document.querySelector('.noOfLifts').value
+const nFloors = document.querySelector('.noOfFloors')
+const nLifts = document.querySelector('.noOfLifts')
 const floors = document.querySelector('.floors')
 const lifts = document.querySelector('.lifts')
 const generateBtn = document.querySelector('.generate')
@@ -8,18 +8,21 @@ const generateBtn = document.querySelector('.generate')
 
 // function to create lifts and floors
 function generate(NoOffloors, NoOflifts) {
-    console.log('construction started')
+
+    floors.innerHTML = ''
+    lifts.innerHTML = ''
     let floorCount = 0
     let liftCount = 0
     while (floorCount < NoOffloors) {
         const floor = document.createElement('div')
         floor.classList.add('floor')
         floors.appendChild(floor)
+        createButtons(floorCount, floor, NoOffloors)
         floorCount++
     }
     while (liftCount < NoOflifts) {
         const lift = document.createElement('div')
-        lift.classList.add('floor')
+        lift.classList.add('lift')
         lifts.appendChild(lift)
         liftCount++
     }
@@ -27,7 +30,34 @@ function generate(NoOffloors, NoOflifts) {
 
 }
 
+function createButtons(count, parent, total) {
+    if (count === 0) {
+        const down = document.createElement('button')
+        down.classList.add('down')
+        down.innerText = 'down';
+        parent.appendChild(down)
+    }
+    else if (count === total - 1) {
+        console.log(count, total)
+        const up = document.createElement('button')
+        up.classList.add('up')
+        up.innerText = 'up';
+        parent.appendChild(up)
+    }
+    else {
+        const down = document.createElement('button')
+        down.classList.add('down')
+        down.innerText = 'down';
+        const up = document.createElement('button')
+        up.classList.add('up')
+        up.innerText = 'up'
+        parent.append(up, down)
+    }
+}
+
 //function call
 generateBtn.addEventListener('click', (e) => {
-    generate(nFloors, nLifts)
+    const n = nFloors.value
+    const m = nLifts.value
+    generate(n, m)
 })
