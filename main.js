80e6578b322a59b96f7a267df481f9161acd4f15
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-09-30 21:06:23 +0530

    added logic for unnessecary calls happing to others lifts when there was a lift available

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const nLifts = document.querySelector('.noOfLifts')
 const floors = document.querySelector('.floors')
 const lifts = document.querySelector('.lifts')
 const generateBtn = document.querySelector('.generate')
-// const liftsArr = []
+const toasts = document.querySelector('.toast')
+const liftsArr = []
 
 
 
 // function to create lifts and floors
 function generate(NoOffloors, NoOflifts) {
-
+    const buildingHeight = NoOffloors * 150
     floors.innerHTML = ''
     lifts.innerHTML = ''
     let floorCount = 0
@@ -19,8 +20,10 @@ function generate(NoOffloors, NoOflifts) {
         floor.classList.add('floor')
         floor.id = floorCount + 1
         floor.setAttribute('data-floor', NoOffloors - floorCount - 1)
+        floor.setAttribute('data-lift-on-floor', false)
         floors.appendChild(floor)
         createButtons(floorCount, floor, NoOffloors)
+        if (floorCount == NoOffloors - 1) { floor.setAttribute('data-lift-on-floor', true) }
         floorCount++
     }
     while (liftCount < NoOflifts) {
@@ -29,37 +32,50 @@ function generate(NoOffloors, NoOflifts) {
         lift.setAttribute('data-isfree', 'true')
         lift.setAttribute('data-position', 1)
         lifts.appendChild(lift)
-        // liftsArr.push(lift)
+        liftsArr.push(lift)
         liftCount++
     }
-    document.querySelectorAll('.down').forEach(button => button.addEventListener('click', (e) => {
-        liftMove(e)
-    }))
-    document.querySelectorAll('.up').forEach(button => button.addEventListener('click', (e) => {
-        liftMove(e)
+    document.querySelectorAll('.up-down').forEach(button => button.addEventListener('click', (e) => {
+        let floorHeight = e.target.parentElement.getBoundingClientRect().top
+        if (liftsArr.some((lift, i) => +-liftsArr[i].style.transform.slice(11, -1).replace('px', '') == (buildingHeight - 150) - e.target.parentElement.offsetTop)) {
+            const reqLift = liftsArr.find((lift, i) => +-liftsArr[i].style.transform.slice(11, -1).replace('px', '') == (buildingHeight - 150) - e.target.parentElement.offsetTop)
+            reqLift.setAttribute("data-isfree", "false")
+            reqLift.classList.add('animation')
+            setTimeout(() => {
+                reqLift.setAttribute("data-isfree", "true")
+                reqLift.classList.remove('animation')
+            }, 4000)
+            toast("There is already a lift")
+        }
+
+        else {
+            liftMove(e)
+        }
+        console.log(liftsArr.map((lift, i) => +-liftsArr[i].style.transform.slice(11, -1).replace('px', '')))
+        console.log((buildingHeight - 150) - e.target.parentElement.offsetTop)
     }))
 }
 // function for creating up and down buttons
 function createButtons(count, parent, total) {
     if (count === 0) {
         const down = document.createElement('button')
-        down.classList.add('down')
+        down.classList.add('down', 'up-down')
         down.innerText = 'down';
         parent.appendChild(down)
     }
     else if (count === total - 1) {
         // console.log(count, total)
         const up = document.createElement('button')
-        up.classList.add('up')
+        up.classList.add('up', 'up-down')
         up.innerText = 'up';
         parent.appendChild(up)
     }
     else {
         const down = document.createElement('button')
-        down.classList.add('down')
+        down.classList.add('down', 'up-down')
         down.innerText = 'down';
         const up = document.createElement('button')
-        up.classList.add('up')
+        up.classList.add('up', 'up-down')
         up.innerText = 'up'
         parent.append(up, down)
     }
@@ -67,7 +83,7 @@ function createButtons(count, parent, total) {
 }
 
 function liftMove(e) {
-    const liftsArr = Array.from(document.querySelectorAll('.lift'))
+    // const liftsArr = Array.from(document.querySelectorAll('.lift'))
     // console.log(liftsArr)
     // console.log(e.target.parentElement.getAttribute('data-floor'))
     const calledFloor = +e.target.parentElement.getAttribute('data-floor')
@@ -106,3 +122,12 @@ generateBtn.addEventListener('click', (e) => {
     generate(n, m)
 })
 
+function toast(message) {
+    const notif = document.createElement('div')
+    notif.innerText = message
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 1000)
+}
